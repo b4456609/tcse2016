b2d296c79e9ccb632d46d4342741362e0837193a
Bernie <b4456609@gmail.com>
2016-03-07 10:47:53 +0800

    fix nav touch close nav on mobile device

--- app/scripts/freelancer.js
+++ app/scripts/freelancer.js
@@ -32,6 +32,6 @@ $('body').scrollspy({
 })
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-  //  $('.navbar-toggle:visible').click();
+$('.nav-item').click(function() {
+    $('.navbar-toggle:visible').click();
 });
