bb4297f0db96fb2a034eb534a1ef1443a127f0e0
Bernie <b4456609@gmail.com>
2016-05-01 15:47:11 +0800

    map keelung

--- app/scripts/freelancer.js
+++ app/scripts/freelancer.js
@@ -28,7 +28,7 @@ $(function() {
 // Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
     }).on("focus", ".floating-label-form-group", function() {
         $(this).addClass("floating-label-form-group-with-focus");
     }).on("blur", ".floating-label-form-group", function() {
@@ -45,3 +45,50 @@ $('body').scrollspy({
 $('.nav-item').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$('#keelung-nightmarket').on('show.bs.modal', function() {
+    console.log("modal show");
+    setTimeout(nightmarketMap, 1000);
+});
+
+function nightmarketMap() {
+    var mapOptions = {
+        zoom: 16,
+        scrollwheel: false,
+        draggable: !("ontouchend" in document),
+        center: new google.maps.LatLng(25.1321,121.7444304),
+        styles: [{
+            "stylers": [{
+                "hue": "#1993af"
+            }]
+        }, {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{
+                "lightness": 100
+            }, {
+                "visibility": "simplified"
+            }]
+        }]
+    };
+
+    var mapElement = document.getElementById('nightmarket-map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(25.128556, 121.742833),
+        map: map,
+        title: '基隆廟口'
+    });
+
+    var marker1 = new google.maps.Marker({
+        position: new google.maps.LatLng(25.135122, 121.746055),
+        map: map,
+        title: '長榮桂冠酒店'
+    });
+}
